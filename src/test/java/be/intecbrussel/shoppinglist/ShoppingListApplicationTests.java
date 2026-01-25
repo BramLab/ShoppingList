034d7fd5c1e0ff7f85bc9fa997d8d7e5d1fbaa24
bram-pc <bram.labarque@gmail.com>
2026-01-25 16:20:22 +0100

    start

---
+++ src/test/java/be/intecbrussel/shoppinglist/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
