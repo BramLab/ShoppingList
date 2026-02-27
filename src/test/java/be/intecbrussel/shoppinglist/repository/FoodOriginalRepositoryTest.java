9017069b413f8e9b7799f5f92be5de0971f37d19
oifjq <bram.labarque@gmail.com>
2026-02-27 15:54:39 +0100

    vr-na

--- src/test/java/be/intecbrussel/shoppinglist/repository/FoodOriginalRepositoryTest.java
+++ src/test/java/be/intecbrussel/shoppinglist/repository/FoodOriginalRepositoryTest.java
@@ -8,9 +8,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class FoodOriginalRepositoryTest {
 
     @Autowired
